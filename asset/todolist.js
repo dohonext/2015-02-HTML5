ece6b8508ac16aac834e0d722de6ec18eccaec6b
Dohonext <justwithus@naver.com>
2015-08-04 03:46:28 +0900

    todo 3

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,105 +1,151 @@
-window.addEventListener("load", function() {
-	addList();
-	completeList();
-	deleteList();
+var TODO = {
+	init : function(){
+		this.getAllTodoList();
+		$('input').on('keydown', this.addList);
+		$("ul").on("click", "li  div  input[type=checkbox]", this.completeList);
+		$("ul").on("click", "li  div  .destroy", this.removeList);
+	},
 	
-}, false);
+	getAllTodoList : function(){
+		TODOSync.get(function(json){
+			for(i=json.length-1; i>=0; i--) {
+				appendWithTemplateEngine("#addListElements", "#todo-list", { key:json[i].id, class:'added', text:json[i].todo});
+			}
+		})
 
+		function appendWithTemplateEngine(template, elementToAppend, dataToBind) {
+		    		var source   = $(template).html();
+					var template = Handlebars.compile(source);
+					var templateData = dataToBind;
 
+					$(elementToAppend).append(template(templateData));	
+		}
+	},
 
-function addList() {
-	$('input').on('keydown', function(e) {
-	    if (e.which == 13) {
-	    	var textValue = $("input").val();
+	addList : function(e){
+		if (e.which == 13) {
+			var textValue = $("input").val();
+			TODOSync.add(textValue, function(json){
+		  	   	appendWithTemplateEngine("#addListElements", "#todo-list", { key:json.insertId, class:'added', text:textValue });
+		  	   	animateFadeIn($("#todo-list li:last-child"));
+		  	   	deleteTextAfterPutIn();
+		    	e.preventDefault();
 
-	    	/*          to append with strings. (this code is changed by template engine.)
-	  	   	$("#todo-list").append("<li class='{}'><div class='view'><input class='toggle' type='checkbox' {}><label>"+textValue+"</label><button class='destroy'></button></div></li>");
-	  	   	*/
-	  	   	appendWithTemplateEngine("#addListElements", "#todo-list", { class:'added', text:textValue });
-	  	   	animateFadeIn($("#todo-list li:last-child"));
-	  	   	deleteTextAfterPutIn();
-	    	e.preventDefault();
+		    	function appendWithTemplateEngine(template, elementToAppend, dataToBind) {
+		    		var source   = $(template).html();
+					var template = Handlebars.compile(source);
+					var templateData = dataToBind;
 
+					$(elementToAppend).append(template(templateData));	
+		    	}
 
+		    	function animateFadeIn(elementToAnimate) {
+					var i = 0;
+					animation();
 
-	    	function appendWithTemplateEngine(template, elementToAppend, dataToBind) {
-	    		var source   = $(template).html();
-				var template = Handlebars.compile(source);
-				var templateData = dataToBind;
+					function animation() {
+						if (i === 50) {
+							return;
+						} else {
+							elementToAnimate.css( "opacity", 0+i*0.2);
+							i++;
+						}
+						requestAnimationFrame(animation);
+					}		
+				}
 
-				$(elementToAppend).append(template(templateData));	
-	    	}
+		    	function deleteTextAfterPutIn() {
+		    		$("input").val('');  
+		    	}
+	    	}.bind(this));
+	    }
+	},
 
-	    	function animateFadeIn(elementToAnimate) {
+	completeList : function(e){
+		var input = e.currentTarget;
+		var elLi = input.closest("li");
+		var completed = input.checked?"1":"0";
+
+		TODOSync.completed({
+			"key" : elLi.dataset.key,
+			"completed" : completed
+		},function(){
+				if (completed==="1") {
+				elLi.className = "completed"
+			}
+			else {
+				elLi.className = "added"
+			}
+		})
+	},
+
+	removeList : function(e){
+		var elLi = e.currentTarget.closest("li");
+
+		TODOSync.remove(elLi.dataset.key, function(){		
+			animateFadeOut($(elLi));
+
+			function animateFadeOut(elementToAnimate) {
 				var i = 0;
 				animation();
 
 				function animation() {
 					if (i === 50) {
+						elementToAnimate.remove();
 						return;
 					} else {
-						elementToAnimate.css( "opacity", 0+i*0.2);
+						elementToAnimate.css( "opacity", 1-i*0.2);
 						i++;
 					}
 					requestAnimationFrame(animation);
 				}		
 			}
-
-	    	function deleteTextAfterPutIn() {
-	    		$("input").val('');  //to delete the text value in a input box after put it in.
-	    	}
-	    }
-	});
+		})
+	},
 };
 
-function completeList() {
-	//to use 'event delegation' to add event listner to newly added DOM. 
-	$("ul").on("click", "li  div  input[type=checkbox]", function(e){
-		
-		//e.delegateTarget = "ul"
-		//e.currentTarget = "input"
-		if (e.currentTarget.checked) {
-			e.currentTarget.closest("li").className = "completed"
-		}
-		else {
-			e.currentTarget.closest("li").className = "added"
-		}
-	});
-};
-
-function deleteList() {
-	/*          to fade out with css transition. (this code is changed by requestAnimationFrame)
- 	$("ul").on("click", "li  div  .destroy", function(e){ 
-		var elLi = e.currentTarget.closest("li");
-		elLi.className = "deleting";
-		elLi.addEventListener("transitionend", function(e) {
-			e.currentTarget.closest("li").remove();
+var TODOSync = {
+	get : function(callback){
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "http://128.199.76.9:8002/dohonext", true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+		xhr.addEventListener("load",function(e) {
+			callback(JSON.parse(xhr.responseText));
 		});
-	}); 
-	*/
+		xhr.send();
+	},
 
-	$("ul").on("click", "li  div  .destroy", function(e){ 
-		var elLi = e.currentTarget.closest("li");
+	add : function(todo, callback){
+		var xhr = new XMLHttpRequest();
+		xhr.open("PUT", "http://128.199.76.9:8002/dohonext", true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+		xhr.addEventListener("load",function(e) {
+			callback(JSON.parse(xhr.responseText));
+		});
+		xhr.send("todo="+todo);
+	},
 
-		animateFadeOut($(elLi));
+	completed : function(param, callback){
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://128.199.76.9:8002/dohonext/"+param.key, true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+		xhr.addEventListener("load",function(e) {
+			callback(JSON.parse(xhr.responseText));
+		});
+		xhr.send("completed="+param.completed);
+	},
 
-		function animateFadeOut(elementToAnimate) {
-			var i = 0;
-			animation();
-
-			function animation() {
-				if (i === 50) {
-					elementToAnimate.remove();
-					return;
-				} else {
-					elementToAnimate.css( "opacity", 1-i*0.2);
-					i++;
-				}
-				requestAnimationFrame(animation);
-			}		
-
-		}
-	});
+	remove : function(key, callback){
+		var xhr = new XMLHttpRequest();
+		xhr.open("DELETE", "http://128.199.76.9:8002/dohonext/"+key, true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+		xhr.addEventListener("load",function(e) {
+			callback(JSON.parse(xhr.responseText));
+		});
+		xhr.send();
+	}
 }
 
-
+window.addEventListener("load", function() {	
+	TODO.init();	
+}, false);
